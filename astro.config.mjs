737fcfffd682387ebb4b65593fc830a447f51746
garbaCoding <garbacoding@gmail.com>
2023-10-07 07:42:17 +0200

    Add UI Framework component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-bien-ce-framework.netlify.app"
+  site: "https://astro-bien-ce-framework.netlify.app",
+  integrations: [preact()]
 });
